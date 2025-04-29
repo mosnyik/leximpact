5f2ec7327566f5198cbc5216767911a8a014566d
Nyikwagh Moses <mosnyik@gmail.com>
2025-04-29 10:27:51 +0100

    test(config): configured vites fo testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
 
 export default async function Home() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      {/* Arrange your components here
+    Navbar
+    main
+    footer
+    Note I left the boilerplate code in place for you to see how it works.
+    I would remove it when it is time */}
+      {/* <Hero /> */}
+      {/* <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      </main> */}
     </>
   );
 }
