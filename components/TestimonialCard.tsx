95ba18a7a59dee78edd2c894e9ffdeda351d28ca
Nyikwagh Moses <mosnyik@gmail.com>
2025-05-02 17:32:01 +0100

    feat(comvoices):testimonial updated dynamically

--- components/TestimonialCard.tsx
+++ components/TestimonialCard.tsx
@@ -11,13 +11,13 @@ export default function TestimonialCard({
 }: TestimonialCardProps) {
   return (
     <div
-      className={`w-[480px] h-[240px] bg-white rounded-[12px] shadow-[2px_2px_5px_1px_rgba(0,0,0,0.18)] p-6 mt-12 flex flex-col justify-between border`}
+      className={`max-w-[480px] max-h-60 bg-white rounded-[12px] shadow-[2px_2px_5px_1px_rgba(0,0,0,0.18)] p-6 mt-12 flex flex-col justify-center border`}
     >
-      <p className="text-[#4B5563] text-base font-montserrat">{text}</p>
-      <div className="flex items-center gap-3 mt-4">
-        <div className="w-6 h-6 bg-[#021488] rounded-full" />
+      <p className="text-[#4B5563]  text-base font-montserrat mb-6">{text}</p>
+      <div className="flex items-center gap-3">
+        <div className="w-6 h-6 bg-[#0546B6] rounded-full" />
         <div>
-          <p className="text-[#021488] font-semibold font-montserrat">{name}</p>
+          <p className="text-[#6B7280] font-semibold font-montserrat">{name}</p>
           <p className="text-sm text-[#6B7280] font-montserrat">{role}</p>
         </div>
       </div>
