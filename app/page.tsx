c92c80c9a20cadd7c8ea6820ac7f23627b34d27a
Nyikwagh Moses <mosnyik@gmail.com>
2025-05-13 00:10:06 +0100

    fix(git):Track Hero.tsx file properly

--- app/page.tsx
+++ app/page.tsx
@@ -15,18 +15,6 @@ export default async function Home() {
         <GetStartedSection />
         <Footer />
       </div>
-
-      {/* Arrange your components here
-    Navbar
-    main
-    footer
-    Note I left the boilerplate code in place for you to see how it works.
-    I would remove it when it is time */}
-      {/* <Hero /> */}
-      {/* <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main> */}
     </>
   );
 }
